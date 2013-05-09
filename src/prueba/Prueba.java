85643fcd7493d0c2a68ce9420a14c62328da04af
Juan David Patiño² <juandavid162@gmail.com>
2013-05-09 18:07:40 -0500

    Update Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -14,6 +14,6 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Vamos a generar un conflicto!!!");
     }
 }
